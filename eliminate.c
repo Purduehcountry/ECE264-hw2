78acadc646ec204490cc469ccc5dc97e1bf8e3f4
Henry Allen Countryman-Dustin <hcountry@eceprog3.ecn.purdue.edu>
2026-01-17 14:19:53 -0500

    Upload

---
+++ eliminate.c
@@ -0,0 +1,73 @@
+/* You MUST modify this file */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> 
+#include <string.h> 
+
+#ifdef TEST_ELIMINATE
+// 100% of the score
+void eliminate(int n, int k)
+{
+  // allocate an arry of n elements
+  int * arr = malloc(sizeof(* arr) * n);
+  // check whether memory allocation succeeds.
+  // if allocation fails, stop
+  if (arr == NULL)
+    {
+      fprintf(stderr, "malloc fail\n");
+      return;
+    }
+	
+  // Note that from here on, you can access elements of the arr with
+  // expressions like a[i]
+	
+  // initialize all elements
+  for(int j = 0; j < n; j++) {
+    arr[j] = j;
+  }
+
+  int pplplaying = n;
+  int index = 0;
+  int count;
+  int saveidx;
+
+  while(pplplaying > 1) {
+    count = 0;
+    while(count < k) {
+      if(arr[index] != 'X') {
+        count++;
+        saveidx = index;
+      }
+      index++;
+      if(index == n) {
+        index = 0;
+      }
+    }
+    printf("%d\n", arr[saveidx]);
+    arr[saveidx] = 'X';
+    pplplaying--;
+  }
+  for(int i = 0; i < n; i++) {
+    if(arr[i] != 'X') {
+      printf("%d", arr[i]);
+    }
+  }
+  
+  // counting to k,
+  // mark the eliminated element
+  // print the index of the marked element
+  // repeat until only one element is unmarked
+
+
+
+
+  // print the last one
+
+
+
+
+  // release the memory of the array
+  free (arr);
+}
+#endif
